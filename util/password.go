4c1a62e9bbafba7ff10a5e6adaedcc7d625be645
Aashish Ailawadi <64075567+aashishAil@users.noreply.github.com>
2024-05-05 12:40:27 +0530

    chore: added util for password

---
+++ util/password.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"github.com/pkg/errors"
+	"golang.org/x/crypto/bcrypt"
+
+	"loanManagement/logger"
+)
+
+type Password interface {
+	Hash(password string) (string, error)
+}
+
+type password struct{}
+
+func (p *password) Hash(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		logger.Log.Error(err.Error())
+		return "", errors.Wrap(err, "failed to hash password")
+	}
+
+	return string(bytes), nil
+}
+
+func NewPassword() Password {
+	return &password{}
+}
