9746258164720e3364221921ab512a01828e1c8e
Aashish Ailawadi <64075567+aashishAil@users.noreply.github.com>
2024-06-03 22:18:18 +0530

    chore: add default admin and customer user(s)

---
+++ database/migration/20240603164331_add_default_users.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "user"(name, email, encrypted_password)
+VALUES ('admin', 'admin@lm.com', '$2a$14$AXoiGB/3i4ctDO/KNiXN6.kLytfajU6vj1TA2zJukvXcU8hRf4v8q'),
+       ('customer 1', 'customer@lm.com', '$2a$14$EZp0uQeFvQIPO1uGzMvPu.6Um99Y25SEvWcTmXGRLMoRZKPijtMQu');
